eff4bbd75cfd4f412b6ebfec825f04f73368e706
CuboidMagician <96688819+ShashCode2348@users.noreply.github.com>
2023-02-22 19:26:28 +0000

    Create Class.cpp

---
+++ Code/Classes/Class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Smth.h"
+using namespace std;
+
+void Speak::qwerty() {
+	cout << "Hello World" << endl;
+	cout << "Bwuh" << endl;
+}
+
+void Speak::hi() {
+	cout << "Hi" << endl;
+}
