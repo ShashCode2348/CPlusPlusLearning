7fd690dfe790eff2ecc15de588376875ae362646
CuboidMagician <96688819+ShashCode2348@users.noreply.github.com>
2023-02-22 19:40:27 +0000

    Create IntegerMemorySizes.cpp

---
+++ Code/IntegerMemorySizes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+# include <limits>
+using namespace std;
+
+int main() {
+	cout << "In bytes: " << endl;
+	cout << "Size of type signed char is " << sizeof(signed char) << endl;
+	cout << "Size of type unsigned char is " << sizeof(unsigned char) << endl;
+	cout << "Size of type char is " << sizeof(char) << endl;
+	cout << "Size of type short int is " << sizeof(short int) << endl;
+	cout << "Size of type unsigned short int is " << sizeof(unsigned short int) << endl;
+	cout << "Size of type int is " << sizeof(int) << endl;
+	cout << "Size of type unsigned int is " << sizeof(unsigned int) << endl;
+	cout << "Size of type long int is " << sizeof(long int) << endl;
+	cout << "Size of type unsigned long int is " << sizeof(unsigned long int) << endl;
+	cout << "Size of type long long int is " << sizeof(long long int) << endl;
+	cout << "Size of type unsigned long long int is " << sizeof(unsigned long long int) << endl;
+}
